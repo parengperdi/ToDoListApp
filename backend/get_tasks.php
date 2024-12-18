622c27b0b79c991ecd5285f4317bd26f01e71fd1
Ferdi-san <coronel_f@bu.glue-si.com>
2024-12-18 16:26:03 +0700

    fix the array initialization

--- backend/get_tasks.php
+++ backend/get_tasks.php
@@ -4,11 +4,13 @@ include 'db.php';
 header("Content-Type: application/json");
 
 $result = $conn->query("SELECT * FROM tasks");
-$tasks = array();
+$tasks = [];
 
+// Fetch all tasks and store them in an array
 while ($row = $result->fetch_assoc()) {
     $tasks[] = $row;
 }
 
+// Return the tasks as JSON
 echo json_encode($tasks);
 ?>
